56dcbb63daedd6a4a45b90bf059f6850300434af
Mike Smith <mike.smith@aspiaspace.com>
2025-08-18 11:19:28 +0100

    Rejigging upstream code

---
+++ src/pu/metrics.py
@@ -0,0 +1,26 @@
+import numpy as np
+from sklearn.neighbors import NearestNeighbors
+
+from pu.zoom import resize_galaxy_to_fit
+
+
+def mknn(Z1, Z2, k=10):
+    """
+    Calculate mutual k nearest neighbours
+    """
+    assert len(Z1) == len(Z2)
+
+    nn1 = (
+        NearestNeighbors(n_neighbors=k, metric="cosine")
+        .fit(Z1)
+        .kneighbors(return_distance=False)
+    )
+    nn2 = (
+        NearestNeighbors(n_neighbors=k, metric="cosine")
+        .fit(Z2)
+        .kneighbors(return_distance=False)
+    )
+
+    overlap = [len(set(a).intersection(b)) for a, b in zip(nn1, nn2)]
+
+    return np.mean(overlap) / k
--- src/pu/preprocess.py
+++ src/pu/preprocess.py
@@ -1,9 +1,9 @@
 import torch
 from astropt.local_datasets import GalaxyImageDataset
 from torchvision import transforms
+import numpy as np
 
-from pu.pu import flux_to_pil
-
+from pu.zoom import resize_galaxy_to_fit
 
 class PreprocessHF:
     """Preprocessor that converts galaxy images to the format expected by Dino and ViT models"""
@@ -15,10 +15,15 @@ class PreprocessHF:
     def __call__(self, idx):
         result = {}
         for mode in self.modes:
-            im = flux_to_pil(idx[f"{mode}_image"], mode, self.modes)
-            result[f"{mode}"] = self.autoproc(im, return_tensors="pt")[
-                "pixel_values"
-            ].squeeze()
+            if (mode != "desi") and (mode != "sdss"): 
+                try:
+                    im = flux_to_pil(idx[f"{mode}_image"], mode, self.modes)
+                except KeyError as e:
+                    # Assume the dataset does not name the images by modality
+                    im = flux_to_pil(idx["image"], mode, self.modes)
+                result[f"{mode}"] = self.autoproc(im, return_tensors="pt")[
+                    "pixel_values"
+                ].squeeze()
 
         return result
 
@@ -47,11 +52,63 @@ class PreprocessAstropt:
     def __call__(self, idx):
         result = {}
         for mode in self.modes:
-            im = flux_to_pil(idx[f"{mode}_image"], mode, self.modes).swapaxes(0, 2)
-            im = self.galproc.process_galaxy(torch.from_numpy(im).to(torch.float)).to(
-                torch.float
-            )
-            result[f"{mode}_images"] = im
-            result[f"{mode}_positions"] = torch.arange(0, len(im), dtype=torch.long)
+            if (mode != "desi") and (mode != "sdss"): 
+                try:
+                    im = flux_to_pil(idx[f"{mode}_image"], mode, self.modes).swapaxes(0, 2)
+                except KeyError as e:
+                    # Assume the dataset does not name the images by modality
+                    im = flux_to_pil(idx[f"image"], mode, self.modes).swapaxes(0, 2)
+                im = self.galproc.process_galaxy(torch.from_numpy(im).to(torch.float)).to(
+                    torch.float
+                )
+                result[f"{mode}_images"] = im
+                result[f"{mode}_positions"] = torch.arange(0, len(im), dtype=torch.long)
 
         return result
+
+
+def flux_to_pil(blob, mode, modes):
+    """
+    Convert raw fluxes to PIL imagery
+    """
+
+    def _norm(chan):
+        scale = np.percentile(chan, 99) - np.percentile(chan, 1)
+        chan = np.arcsinh((chan - np.percentile(chan, 1)) / scale)
+        #v0, v1 = np.nanpercentile(chan, 5), np.nanpercentile(chan, 99)
+        #chan = ((chan - v0) / (v1 - v0)).clip(0, 1) * 255
+        chan = (chan - chan.min()) / (chan.max() - chan.min())
+        return chan
+
+    arr = np.asarray(blob["flux"], np.float32)
+    if mode == "hsc":
+        if arr.ndim == 3:
+            arr = np.stack([arr[2], arr[2], arr[2]], axis=-1)  # gri
+        elif arr.ndim == 2:
+            arr = np.stack([arr, arr, arr], axis=-1)
+        if ("jwst" in modes) or ("desi" in modes) or ("sdss" in modes):
+            # if comparing hsc to jwst resize hsc so it matches hsc
+            arr = resize_galaxy_to_fit(
+                arr, force_extent=(68, 92, 68, 92), target_size=96
+            )
+    if (
+        mode == "jwst"
+    ):  # 0.04 pixel per arcsec https://dawn-cph.github.io/dja/blog/2023/07/18/image-data-products/
+        if arr.ndim == 3:
+            arr = np.stack([arr[1], arr[3], arr[6]], axis=-1)  #
+        elif arr.ndim == 2:
+            arr = np.stack([arr, arr, arr], axis=-1)
+    if mode == "legacysurvey":
+        if arr.ndim == 3:
+            arr = np.stack([arr[0], arr[1], arr[3]], axis=-1)  # grz
+        elif arr.ndim == 2:
+            arr = np.stack([arr, arr, arr], axis=-1)
+        # we always resize legacy to match hsc for our use-case
+        arr = resize_galaxy_to_fit(
+            arr, force_extent=(36, 124, 36, 124), target_size=160
+        )
+
+    arr = _norm(arr)#np.stack([_norm(arr[..., ii]) for ii in range(3)], axis=-1)
+    arr = (arr * 255).astype(np.uint8)
+
+    return arr
